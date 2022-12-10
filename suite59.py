524ff4da53918f49cc77d42ce0640a6788b5c98f
Adri1l <117633258+Adri1l@users.noreply.github.com>
2022-12-10 19:29:23 +0100

    Update and rename suite58.py to suite59.py

--- suite59.py
+++ suite59.py
@@ -63,7 +63,7 @@ def update():
         deplacement_vertical = -3 
 
 
-    if vaisseau_y <= balle_y +6 <= vaisseau_y   and vaisseau_x + 24 <= balle_x <=vaisseau_x + 25 + 16 :
+    if vaisseau_y <= balle_y +6 <= vaisseau_y   and vaisseau_x + 24 <= balle_x <=vaisseau_x + 25  :
         deplacement_vertical = -3 
         if deplacement_horizontal == -2 or deplacement_horizontal == 0:
             deplacement_horizontal = 1   
